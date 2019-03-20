a300c38ebe730352122d1e62af6accf0cc0e7f26
Denis Washington <denis@denisw.de>
2019-03-20 08:29:26 +0100

    Add API docs for chainReducers

--- docs/api/chainReducers.md
+++ docs/api/chainReducers.md
@@ -1,1 +1,68 @@
 # `chainReducers`
+
+Creates a reducer that chains together a sequence of (sub-)reducers.
+
+```js
+// JavaScript
+
+function chainReducers(firstChildReducer, ...otherChildReducers)
+```
+
+```ts
+// TypeScript
+
+function chainReducers<S = any, A extends Action = AnyAction>(
+  firstChildReducer: Reducer<S, A>,
+  ...otherChildReducers: SubReducer<S, A>[]
+): Reducer<S, A>
+```
+
+## Details
+
+Given a sequence of (sub-)reducers, `chainReducers` creates a reducer which
+forwards incoming actions to each of these "child" reducers. More
+specifically, the reducer: 
+
+1. calls the first child reducer with the received state
+2. passes the resulting state (and the same action) to the next reducer
+3. repeats step 2 until reaching the last reducer, whose returned state is
+   finally returned by the `chainReducers` reducer.
+
+Note that the first child reducer in the chain is the only one which needs to
+handle an `undefined` state by returning the initial state; all others can be
+*sub-reducers* - such as the ones returned by [`onAction`](./onAction.md) -
+which assume that they are only ever called with a defined,
+already-initialized state. 
+
+## Examples
+
+Chaining `withInitialState` and `onAction` reducers:
+
+```js
+import { 
+  chainReducers, 
+  onAction, 
+  withInitialState 
+} from 'redux-preboiled'
+
+const reducer = chainReducers(
+  withInitialState(0),
+  onAction('increment', state => state + 1),
+  onAction('multiply', (state, { payload }) => state * payload)
+)
+
+reducer(undefined, { type: '' })
+// => 0
+
+reducer(0, { type: 'increment' })
+// => 1
+
+reducer(2, { type: 'multiply', payload: 4 })
+// => 8
+```
+
+## See Also
+
+* [Reducers](../guide/reducers.md) guide
+* [onAction](./onAction.md)
+* [withInitialState](./withInitialState.md)
--- src/chainReducers.ts
+++ src/chainReducers.ts
@@ -2,34 +2,33 @@ import { Action, AnyAction, Reducer } from 'redux'
 import { SubReducer } from './types.js'
 
 /**
- * Returns a reducer that calls the passed child reducers in sequence.
+ * Returns a reducer that calls the passed (sub-)reducers in sequence.
  * That is, when the reducer is called, it passes the input state and
  * action to the first child reducer, then the returned state together
  * with the same action to the second child reducer, and so on. Finally,
  * the state returned by the last child reducer is returned by the
  * chained reducer itself.
  *
- * A common pattern is to specify `withInitialState(initialState)` as
- * the first child reducer, followed by a series of `onAction()`
- * reducers for different action types.
- *
- * @param firstReducer - The first child reducer to call.
- * @param otherReducers - The other child reducers to chain together.
+ * @param firstChildReducer - The first (sub-)reducer in the chain.
+ * @param otherChildReducers - The other (sub-)reducers to chain together.
  */
 export default function chainReducers<S = any, A extends Action = AnyAction>(
-  firstReducer: Reducer<S, A>,
-  ...otherReducers: SubReducer<S, A>[]
+  firstChildReducer: Reducer<S, A>,
+  ...otherChildReducers: SubReducer<S, A>[]
 ): Reducer<S, A> {
-  if (!firstReducer) {
+  if (!firstChildReducer) {
     throw new Error('chainReducers() needs at least one reducer.')
   }
 
-  if (otherReducers.length === 0) {
-    return firstReducer
+  if (otherChildReducers.length === 0) {
+    return firstChildReducer
   }
 
   return function chainedReducer(state, action) {
-    const nextState = firstReducer(state, action)
-    return otherReducers.reduce((s, reducer) => reducer(s, action), nextState)
+    const nextState = firstChildReducer(state, action)
+    return otherChildReducers.reduce(
+      (s, reducer) => reducer(s, action),
+      nextState
+    )
   }
 }
