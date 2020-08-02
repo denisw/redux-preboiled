7384bec88325f5278c3f2c7747e16e3011c45f6c
Judith Hartmann <judith.hartmann@futurice.com>
2020-08-02 14:24:41 +0200

    Fix onAction: Treat symbol + numbers as type

--- test/onAction.test.ts
+++ test/onAction.test.ts
@@ -14,17 +14,54 @@ beforeEach(() => {
 })
 
 describe('with action type', () => {
-  test('calls action reducer for matching actions', () => {
-    const reducer = onAction('increment', onIncrement)
-    const nextState = reducer(0, increment())
-    expect(nextState).toBe(1)
+  describe('as string', () => {
+    test('calls action reducer for matching actions', () => {
+      const reducer = onAction('increment', onIncrement)
+      const nextState = reducer(0, increment())
+      expect(nextState).toBe(1)
+    })
+
+    test('returns unchanged state for non-matching actions', () => {
+      const reducer = onAction('increment', onIncrement)
+      const nextState = reducer(0, multiply(2))
+      expect(nextState).toBe(0)
+      expect(onIncrement).not.toHaveBeenCalled()
+    })
   })
 
-  test('returns unchanged state for non-matching actions', () => {
-    const reducer = onAction('increment', onIncrement)
-    const nextState = reducer(0, multiply(2))
-    expect(nextState).toBe(0)
-    expect(onIncrement).not.toHaveBeenCalled()
+  describe('as number', () => {
+    const incrementNumberAction = createAction(1)
+
+    test('calls action reducer for matching actions', () => {
+      const reducer = onAction(1, onIncrement)
+      const nextState = reducer(0, incrementNumberAction())
+      expect(nextState).toBe(1)
+    })
+
+    test('returns unchanged state for non-matching actions', () => {
+      const reducer = onAction(1, onIncrement)
+      const nextState = reducer(0, multiply(2))
+      expect(nextState).toBe(0)
+      expect(onIncrement).not.toHaveBeenCalled()
+    })
+  })
+
+  describe('as symbol', () => {
+    const incrementSymbol = Symbol('increment')
+    const incrementSymbolAction = createAction(incrementSymbol)
+
+    test('calls action reducer for matching actions', () => {
+      const reducer = onAction(incrementSymbol, onIncrement)
+      const nextState = reducer(0, incrementSymbolAction())
+      expect(nextState).toBe(1)
+    })
+
+    test('returns unchanged state for non-matching actions', () => {
+      const reducer = onAction(incrementSymbol, onIncrement)
+      const nextState = reducer(0, multiply(2))
+      expect(nextState).toBe(0)
+      expect(onIncrement).not.toHaveBeenCalled()
+    })
   })
 })
 
